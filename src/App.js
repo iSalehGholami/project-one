2590b021be6c56a6d4fa215eb01157e2c282f909
Mo. Saleh Gholami <iSalehGholami@gmail.com>
2024-04-26 19:57:35 +0330

    Removing the unused default codes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/header';
+
+// instead of for, we'll use htmlFor
+// instead of class, we'll use className
+// instead of using <Link> we'll use import
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input name='user' />
+      <Header />
     </div>
   );
 }
